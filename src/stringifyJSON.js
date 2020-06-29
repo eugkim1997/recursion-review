441cd6e9570ea728b73696e2b9d3d2bca0b356a5
Eugene Kim <eugkim1997@gmail.com>
2020-06-29 15:23:10 -0700

    Complted JSONstringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,17 +5,25 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  var arr = [];
   if (typeof obj === 'number' || typeof obj === 'boolean') {
     return obj.toString();
   } else if (typeof obj === 'string') {
     return '"' + obj + '"';
   } else if (obj === null) {
-    return 'null';
+    return 'null'
   } else if (Array.isArray(obj)) {
     var arr = [];
     for (var i = 0; i < obj.length; i++) {
       arr.push(stringifyJSON(obj[i]));
     }
     return '[' + arr.join(',') + ']';
-  } else
+  } else if (typeof obj === 'object') {
+    for (var key in obj) {
+      if (typeof obj[key] !== 'function' && typeof obj[key] !== 'undefined') {
+        arr.push('"' + key + '":' + stringifyJSON(obj[key]));
+      }
+    }
+    return '{' + arr.join(',') + '}';
+  }
 };

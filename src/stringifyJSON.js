543291150964f3424ee6ea782da986b3e6ad0ba6
Eugene Kim <eugkim1997@gmail.com>
2020-06-29 12:33:01 -0700

    Stringifies numbers, booleans, strings, arrays

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,17 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (typeof obj === 'number' || typeof obj === 'boolean') {
+    return obj.toString();
+  } else if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  } else if (obj === null) {
+    return 'null';
+  } else if (Array.isArray(obj)) {
+    var arr = [];
+    for (var i = 0; i < obj.length; i++) {
+      arr.push(stringifyJSON(obj[i]));
+    }
+    return '[' + arr.join(',') + ']';
+  } else
 };
